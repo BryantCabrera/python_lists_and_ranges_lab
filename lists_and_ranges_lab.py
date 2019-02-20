5a4f1eed7b9322c0028bf88ab23e1cd3a3435997
Bryant <cabrera.bryant@gmail.com>
2019-02-20 09:54:36 -0800

    Adds solution to Exercise 1.

---
+++ lists_and_ranges_lab.py
@@ -0,0 +1,16 @@
+"""
+Exercise 1
+Create a list named students containing some student names (strings).
+Print out the second student's name.
+Print out the last student's name.
+"""
+students = ['Bryant', 'Michael', 'Kevin', 'Brent', 'Peter', 'Abe', 'Ummer']
+print(students[1])
+print(students[-1])
+
+
+"""
+Exercise 2
+Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+Use a for loop to print out the string "food goes here is a good food"
+"""
